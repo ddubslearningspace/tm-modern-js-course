444f0ea9d735ce4948a8cea16ca86136d9e680a0
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-08-19 15:28:32 -0600

    added general loops

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -509,4 +509,82 @@ todo.delete();
 
 console.clear();
 
+/******************
+ * General Loops
+ *****************/
+
+// FOR LOOP
+for (let i = 0; i < 10; i++) {
+	if (i === 2) {
+		console.log('2 is my favourite number');
+		continue;
+	}
+
+	if (i === 5) {
+		console.log('stop the loop');
+		break;
+	}
+
+	console.log('Number ' + i);
+}
+
+// WHILE LOOP
+let i = 0;
+
+while (i < 10) {
+	console.log('Number ' + i);
+	i++;
+}
+
+// DO WHILE
+do {
+	console.log('Number ' + i);
+	i++;
+} 
+
+while (i < 14)
+
+console.clear();
+
+// Loop through array
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+for (let i = 0; i < cars.length; i++) {
+	console.log(cars[i])
+}
+
+// FOREACH LOOP
+cars.forEach(function(car, index, array){
+	console.log(`${index} : ${car}`);
+});
+
+console.clear();
+
+// MAP
+const users = [
+	{id: 1, name: 'John'},
+	{id: 2, name: 'Sara'},
+	{id: 3, name: 'Karen'},
+	{id: 4, name: 'Steve'}
+]
+
+const ids = users.map(function(user) {
+	return user.id;
+});
+
+val = ids;
+
+// FOR IN LOOP
+const user = {
+	firstName: 'John',
+	lastName: 'Doe',
+	age: 40
+}
+
+for (let x in user) {
+	console.log(`${x} : ${user[x]}`);
+}
+
+console.clear();
+
 console.log(val);
