651a9494cfff937eb37a46f327fba27499e16e33
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-11-09 16:26:26 -0700

    added section 7.7 - callback functions

---
+++ 07_async/7_7_project_files/app.js
@@ -0,0 +1,52 @@
+const posts = [
+	{ title: "Post One", body: "This is post one." },
+	{ title: "Post Two", bosy: "This is post two." },
+];
+
+/**
+ * Synchronous
+ */
+
+// function createPost(post) {
+// 	setTimeout(function () {
+// 		posts.push(post);
+// 	}, 2000);
+// }
+
+// function getPosts() {
+// 	setTimeout(function () {
+// 		let output = "";
+// 		posts.forEach(function (post) {
+// 			output += `<li>${post.title}</li>`;
+// 		});
+
+// 		document.body.innerHTML = output;
+// 	}, 1000);
+// }
+
+// createPost({ title: "Post Three", body: "This is post three." });
+// getPosts();
+
+/**
+ * Asynchronous
+ */
+
+function createPost(post, callBack) {
+	setTimeout(function () {
+		posts.push(post);
+		callBack();
+	}, 2000);
+}
+
+function getPosts() {
+	setTimeout(function () {
+		let output = "";
+		posts.forEach(function (post) {
+			output += `<li>${post.title}</li>`;
+		});
+
+		document.body.innerHTML = output;
+	}, 1000);
+}
+
+createPost({ title: "Post Three", body: "This is post three." }, getPosts);
